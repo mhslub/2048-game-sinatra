ddb87e589a94d758ef1475d2c89b90e93ce1aa80
dexcont <devgroupcont@gmail.com>
2015-02-03 14:48:38 +0800

    Sync with database

--- firstapp/bin/app.rb
+++ firstapp/bin/app.rb
@@ -1,14 +1,33 @@
 require_relative 'contents'
 require_relative 'score'
 require 'sinatra'
+require "data_mapper"
+require "rubygems"
+require  'dm-migrations'
+#require 'process_shared'
+#require_relative 'app1'
+#require_relative 'app2'
 
-set :port, 8080
+#take the port as a parameter when calling
+#set :port , 8299
 set :static, true
 set :public_folder, "public"
 set :views, "views"
 #set :con, Contents.new()
 
-$con =Contents.new()
+DataMapper.setup(:default, 'sqlite3://'+ Dir.pwd+'/bin/contents.db')
+
+class Cons_t
+  include DataMapper::Resource
+  
+  property :id, Serial #key
+  property :mycon, Object
+end
+DataMapper.auto_upgrade!
+DataMapper.finalize
+
+Cons_t.first_or_create(:id=>1).update(:mycon => Contents.new())
+
 $status = "Playing"
 $score_obj=Score.new()
 
@@ -17,78 +36,82 @@ get '/' do
 end
 
 get '/game/' do
+ 
+   obj= Cons_t.get(1).mycon
   
   case params[:dir]
   when "1"
     if $status == "Playing"
-      $con.moveUp($score_obj)
-      if($con.detectGameOver())
+      obj.moveUp($score_obj)
+      if(obj.detectGameOver())
         $status ="Game Over!"
       end    
-      if($con.detectWin())
+      if(obj.detectWin())
         $status ="You Win!"
       end
     end
-    @ary = $con.contents
+    @ary = obj.contents
     @msg=$status
     @alr="Press 'Enter' for a new game."
     @sc=$score_obj.sc
     
   when "2"
     if $status == "Playing"
-      $con.moveRight($score_obj)
-      if($con.detectGameOver())
+      obj.moveRight($score_obj)
+      if(obj.detectGameOver())
         $status ="Game Over!"
       end    
-      if($con.detectWin())
+      if(obj.detectWin())
         $status ="You Win!"
       end
     end
-    @ary = $con.contents
+    @ary = obj.contents
     @msg=$status
     @alr="Press 'Enter' for a new game."
     @sc=$score_obj.sc
     
   when "3"
     if $status == "Playing"
-      $con.moveLeft($score_obj)
-      if($con.detectGameOver())
+      obj.moveLeft($score_obj)
+      if(obj.detectGameOver())
         $status ="Game Over!"
       end    
-      if($con.detectWin())
+      if(obj.detectWin())
         $status ="You Win!"
       end
     end
-    @ary = $con.contents
+    @ary = obj.contents
     @msg=$status
     @alr="Press 'Enter' for a new game."
     @sc=$score_obj.sc
    
   when "4"
     if $status == "Playing"
-      $con.moveDown($score_obj)
-      if($con.detectGameOver())
+      obj.moveDown($score_obj)
+      if(obj.detectGameOver())
         $status ="Game Over!"
       end    
-      if($con.detectWin())
+      if(obj.detectWin())
         $status ="You Win!."
       end
     end
-    @ary = $con.contents
+    @ary = obj.contents
     @msg=$status
     @alr="Press 'Enter' for a new game."
     @sc=$score_obj.sc
       
   else
     #new game is requested 
-    $con =Contents.new()
+    obj =Contents.new()
     $status = "Playing"
     $score_obj=Score.new()
-    @ary = $con.contents
+    @ary = obj.contents
     @msg=$status
     @alr="Press 'Enter' for a new game."
     @sc=$score_obj.sc
   end
+  
+  Cons_t.get(1).update(:mycon => obj)
   
   #erb is a template that uses the variable mentioned below
   erb :'index' #, :locals => {'ary' => ary , 'msg' =>msg})
